5f0f98b0315fbce6f7654c06b81e7e3ab427c549
Enrico Spinielli <enrico.spinielli@gmail.com>
2021-07-02 11:38:04 +0200

    rename .Rproj file

--- R/ectrl_pub.R
+++ R/ectrl_pub.R
@@ -38,4 +38,5 @@ create_ectrl_pub = function(dirname = "new-dir", template = "ectrl-publication")
       file.path(dirname),
       recursive = TRUE)
    file.rename(file.path(dirname, "skeleton.Rmd"), file.path(dirname, "index.Rmd"))
+   file.rename(file.path(dirname, "xyzzy.Rproj"), file.path(dirname, paste0(dirname, ".Rproj")))
 }
